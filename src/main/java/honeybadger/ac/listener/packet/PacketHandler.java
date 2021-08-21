7439d0bd82accd70241c3f37cc4c701bb789bfcf
Salers <salersaimlisse@gmail.com>
2021-08-21 23:37:43 +0200

    Update PacketHandler.java

--- src/main/java/honeybadger/ac/listener/packet/PacketHandler.java
+++ src/main/java/honeybadger/ac/listener/packet/PacketHandler.java
@@ -118,8 +118,10 @@ public class PacketHandler {
 
             // PLAYER DATA
         } else if (event.getPacketType() == PacketType.Play.Client.BLOCK_DIG) {
+
             final WrapperPlayClientBlockDig wrapper = new WrapperPlayClientBlockDig(event.getPacket());
             final boolean isSolid = wrapper.getLocation().toLocation(event.getPlayer().getWorld()).getBlock().getType().isSolid();
+            
             if (isSolid && event.getPacket().getPlayerDigTypes().read(0).equals(EnumWrappers.PlayerDigType.START_DESTROY_BLOCK)) {
                 data.getInteractionData().setDigging(true);
             } else if (event.getPacket().getPlayerDigTypes().read(0).equals(EnumWrappers.PlayerDigType.ABORT_DESTROY_BLOCK)
